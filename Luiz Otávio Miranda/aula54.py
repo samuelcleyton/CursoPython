2671910f26ffeef6b2ee4abd33038288dba098c7
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2023-02-15 18:04:46 -0300

    up

---
+++ Luiz Otávio Miranda/aula54.py
@@ -0,0 +1,10 @@
+"""
+Faça uma lista de comprar com listas
+O usuário deve ter a possibilidade de
+inserir, apagar e listar valores da sua lista
+Não permita que o programa quebre com 
+erros de índices inexistentes na lista.
+"""
+
+
+    
