28344fe09476770604a7a74f2e6078935dfe17e7
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2023-02-19 17:59:39 -0300

    up

--- Luiz Otávio Miranda/aula61.py
+++ Luiz Otávio Miranda/aula61.py
@@ -23,35 +23,72 @@ Se o resultado anterior for maior que 9:
 contrário disso:
     resultado é o valor da conta
 O primeiro dígito do CPF é 7
+
+****************************
+
+Calculo do segundo dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF,
+MAIS O PRIMEIRO DIGITO,
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 11
+Ex.:  746.824.890-70 (7468248907)
+   11 10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
+   77 40 54 64 14 24 40 36  0 14
+Somar todos os resultados:
+77+40+54+64+14+24+40+36+0+14 = 363
+Multiplicar o resultado anterior por 10
+363 * 10 = 3630
+Obter o resto da divisão da conta anterior por 11
+3630 % 11 = 0
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+O segundo dígito do CPF é 0
+
 """
 
-cpf = input('Digite os 9 primeiros digitos do seu CPF: ')
-print(cpf.split())
+cpf = input('Digite o seu CPF: ')
+nove_digitos = cpf[:9]
 
 
+count_1 = 10
+soma_1 = 0
 
-i = 10
-soma = 0
+count_2 = 11
+soma_2 = 0
 
 try:
-    cpf_int = int(cpf)
+    cpf_int = int(nove_digitos)
+    novo_cpf = [nove_digitos]
+
+    for digitos in nove_digitos:
+        multi_1 = int(digitos) * count_1
+        soma_1 = soma_1 + multi_1
+        count_1 = count_1 - 1
+
+    for numeros in novo_cpf:
+        multi_2 = int(numeros) * count_2
+        soma_2 = soma_2 + multi_2
+        count_2 = count_2 - 1
+
+    resultado_1 = (soma_1 * 10) % 11
+    resultado_2 = (soma_2 * 10) % 11
     
-    for digito in cpf:
-        digito_int = int(digito)
-        digito_mult = digito_int * i
-        soma = soma + digito_mult
-        i = i - 1
-
-    resultado = soma * 10 % 11
-
-    cpf_str = str(cpf)
-    novo_cpf = [cpf_str]
-
-    if resultado > 9:
-        novo_cpf.append(str(0))
+    if resultado_1 <= 9:
+        novo_cpf.append(str(resultado_1))
     else:
-        novo_cpf.append(str(resultado))
+        novo_cpf.append('0')
+
+    if resultado_2 > 9:
+        novo_cpf.append('0')
+    else:
+        novo_cpf.append(str(resultado_2))
+
 except:
     print('Digite apenas números!')
 
-print('-'.join(novo_cpf))
+cpf_completo = novo_cpf[0] + novo_cpf[1]
+print(cpf_completo)
--- Python - Nilo Ney/11. Extra.py
+++ Python - Nilo Ney/11. Extra.py
@@ -1,7 +1,42 @@
-n = 1
-soma = 0
-while n <= 10:
-    x = int(input("Digite o %d número:"%n))
-    soma = soma + x
-    n = n + 1 
-print("Soma: %d"%soma)
+cpf = input('Digite o seu CPF: ')
+nove_digitos = cpf[:9]
+
+count_1 = 10
+soma_1 = 0
+
+count_2 = 11
+soma_2 = 0
+
+try:
+    cpf_int = int(nove_digitos)
+    
+    novo_cpf = [nove_digitos]
+
+    for digitos in nove_digitos:
+        multi_1 = int(digitos) * count_1
+        soma_1 = soma_1 + multi_1
+        count_1 = count_1 - 1
+
+    for numeros in novo_cpf:
+        multi_2 = int(numeros) * count_2
+        soma_2 = soma_2 + multi_2
+        count_2 = count_2 - 1
+
+    resultado_1 = (soma_1 * 10) % 11
+    resultado_2 = (soma_2 * 10) % 11
+
+    if resultado_1 <= 9:
+        novo_cpf.append(str(resultado_1))
+    else:
+        novo_cpf.append('0')
+
+    if resultado_2 <= 9:
+        novo_cpf.append(str(resultado_2))
+    else:
+        novo_cpf.append('0')
+
+except:
+    print('Digite apenas números!')
+
+cpf_completo = ''.join(novo_cpf)
+print(cpf_completo)
