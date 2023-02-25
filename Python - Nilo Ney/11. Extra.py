c6ddca8b89d3ecbc770012a7266146e7fe51ac02
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2023-02-25 17:00:19 -0300

    enumerate

--- Python - Nilo Ney/11. Extra.py
+++ Python - Nilo Ney/11. Extra.py
@@ -1,42 +1,6 @@
-cpf = input('Digite o seu CPF: ')
-nove_digitos = cpf[:9]
 
-count_1 = 10
-soma_1 = 0
+L = [10, 9, 8, 7, 6]
 
-count_2 = 11
-soma_2 = 0
-
-try:
-    cpf_int = int(nove_digitos)
+for x, e in enumerate(L):
+    print(f'[{x}] {e}')
     
-    novo_cpf = [nove_digitos]
-
-    for digitos in nove_digitos:
-        multi_1 = int(digitos) * count_1
-        soma_1 = soma_1 + multi_1
-        count_1 = count_1 - 1
-
-    for numeros in novo_cpf:
-        multi_2 = int(numeros) * count_2
-        soma_2 = soma_2 + multi_2
-        count_2 = count_2 - 1
-
-    resultado_1 = (soma_1 * 10) % 11
-    resultado_2 = (soma_2 * 10) % 11
-
-    if resultado_1 <= 9:
-        novo_cpf.append(str(resultado_1))
-    else:
-        novo_cpf.append('0')
-
-    if resultado_2 <= 9:
-        novo_cpf.append(str(resultado_2))
-    else:
-        novo_cpf.append('0')
-
-except:
-    print('Digite apenas números!')
-
-cpf_completo = ''.join(novo_cpf)
-print(cpf_completo)
