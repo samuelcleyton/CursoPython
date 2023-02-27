6e9f476c39d2654d882277febbea4d03ab5a2961
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2023-02-27 15:50:28 -0300

    Introdução ao tipo set em Python (conjuntos)

---
+++ Luiz Otávio Miranda/aula78.0.py
@@ -0,0 +1,11 @@
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.html
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+
+s1 = set() # vazio
+s1 = {'Samuel', 1, 2, 3} # com dados
+
+
