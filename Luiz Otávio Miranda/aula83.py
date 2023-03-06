7261053c0bdb99a0d60b8113d7e69045a2e0c4fb
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2023-03-06 17:21:00 -0300

    Empacotamento e desempacotamento de dicionários + *args e **kwargs

--- Luiz Otávio Miranda/aula83.py
+++ Luiz Otávio Miranda/aula83.py
@@ -21,3 +21,25 @@ a, b = pessoa.items() # retorna chave e valores
 # for chave, valor in pessoa.items():
 #     print(chave, valor)
 
+dados_pessoa = {
+    'idade': 16,
+    'altura': 1.6,
+}
+
+pessoas_completa = {**pessoa, **dados_pessoa}
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print('NÃO NOMEADOS: ', args)
+
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+configurações = {
+    'arg1': 1,
+    'arg2': 2,
+    'arg3': 3,
+    'arg4': 4,
+}
+
+mostro_argumentos_nomeados(**configurações)
+
