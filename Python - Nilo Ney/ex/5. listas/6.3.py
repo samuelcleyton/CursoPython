2ac5bb0ac6ebd6178fb415f9c167c1f334c661a9
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2023-03-08 19:53:48 -0300

    up

---
+++ Python - Nilo Ney/ex/5. listas/6.3.py
@@ -0,0 +1,7 @@
+# Faça um programa que percorra duas listas e gere uma terceira sem elementos repetidos.
+
+lista1 = [1, 2, 3, 2, 1]
+lista2 = [4, 5, 6, 5, 4]
+lista3 = lista2 - lista1
+
+print(lista3)
