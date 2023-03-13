9454d346ad7d9713870354f67fa0f348270dc187
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2023-03-13 17:24:56 -0300

    Mais detalhes sobre Iterables e Iterators (Iteráveis e Iteradores)

---
+++ Luiz Otávio Miranda/aula90.py
@@ -0,0 +1,8 @@
+# Generator expression, Iterables e Iterators em Python
+
+iterable = ['Eu', 'Tenho', '__iter__']
+iterator = iter(iterable)
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
