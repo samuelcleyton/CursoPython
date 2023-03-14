8d93ee0e1d5f505895425d75c1714783836bc25b
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2023-03-14 20:03:50 -0300

    Generator expression, Iterables e Iterators em Python

--- Luiz Otávio Miranda/aula90.py
+++ Luiz Otávio Miranda/aula90.py
@@ -1,5 +1,7 @@
 # Generator expression, Iterables e Iterators em Python
 
+import sys
+
 iterable = ['Eu', 'Tenho', '__iter__']
 iterator = iter(iterable)
 
@@ -7,3 +9,11 @@ iterator = iter(iterable)
 # print(next(iterator))
 # print(next(iterator))
 
+lista = [n for n in range(100000)]
+generator = (n for n in range(100000))
+
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
+
+for n in generator:
+    print(n)
