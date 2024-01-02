b4f0dafc6c26bfe043e5f4fd5e276dc48d528bfb
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2024-01-02 05:11:45 -0300

    Impressão de Fatura

---
+++ Introdução a Ciência da Computação com Python I/Tarefa de programação Lista de exercícios - 1/Exercício 3.py
@@ -0,0 +1,13 @@
+''' Escreva um programa que receba (entrada de dados através do teclado) o nome do cliente, o dia de vencimento, o mês de vencimento e o valor da fatura  e imprima (saída de dados) a mensagem com os dados recebidos, 
+no mesmo formato da mensagem: 
+
+Olá, Fulano de Tal
+A sua fatura com vencimento em 9 de Janeiro no valor de R$ 350,00 está fechada. '''
+
+nome = str(input('Digite o nome do cliente: '))
+dia = int(input('Digite o dia de vencimento: '))
+mês = str(input('Digite o mês de vencimento: '))
+valor = float(input('Digite o valor da fatura: '))
+
+print(f'Olá, {nome}')
+print(f'A sua fatura com vencimento em {dia} de {mês} no valor de R$ {valor:.2f} está fechada.')
