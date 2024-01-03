8394608b00ab0181b0b3aff6e597cbf428ab3756
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2024-01-03 04:27:02 -0300

    Par ou Ímpar?

---
+++ Introdução a Ciência da Computação com Python I/Tarefa de programação Lista de exercícios - 1/Exercício 7.py
@@ -0,0 +1,9 @@
+# Receba um número inteiro na entrada e imprima: par, quando o número for par ou ímpar, quando o número for ímpar.
+
+n = int(input('Digite um número: '))
+
+if n % 2 == 0:
+    print('par')
+else:
+    print('ímpar')
+
