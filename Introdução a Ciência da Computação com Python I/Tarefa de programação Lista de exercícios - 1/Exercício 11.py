bbd36976153ddba95d9680346a923488f728f9ae
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2024-01-03 04:49:19 -0300

    Verificando ordenação

--- Introdução a Ciência da Computação com Python I/Tarefa de programação Lista de exercícios - 1/Exercício 11.py
+++ Introdução a Ciência da Computação com Python I/Tarefa de programação Lista de exercícios - 1/Exercício 11.py
@@ -5,7 +5,7 @@ n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 n3 = int(input('Digite o terceiro número: '))
 
-if n2 == n1 + 1 and n3 == n2 + 1:
+if n1 < n2 < n3:
     print('crescente')
 else:
     print('não está em ordem crescente')
