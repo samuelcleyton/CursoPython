43b62410f253076ac969a162e89d389071c3ed0b
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2024-01-03 05:11:17 -0300

    Fórmula de Bhaskara

--- Introdução a Ciência da Computação com Python I/Tarefa de programação Lista de exercícios - 1/Exercício 6.py
+++ Introdução a Ciência da Computação com Python I/Tarefa de programação Lista de exercícios - 1/Exercício 6.py
@@ -5,19 +5,18 @@ b = float(input('qual é o valor de "b"? '))
 c = float(input('qual é o valor de "c"? '))
 
 delta = b ** 2 - 4 * a * c
-print(f'delta vale {delta}')
 
 if delta > 0:
     x1 = (- b + math.sqrt(delta)) / (2 * a) 
     x2 = (b + math.sqrt(delta)) / (2 * a)
-    print(f'A primeira raiz vale {x1} e a segunda raiz vale {x2}')
+    if x1 < x2: 
+        print(f'as raízes da equação são {x1} e {x2}')
+    else:
+        print(f'as raízes da equação são {x2} e {x1}')
+
 elif delta == 0:
     x1 = -b / (2 * a)
-    print(f'A equação possui apenas 1 raiz real = {x1}')
+    print(f'a raiz desta equação é {x1}')
+
 else:
-    print('A equação não possui raízes reais.')
-
-
-
-
-
+    print('esta equação não possui raízes reais')
