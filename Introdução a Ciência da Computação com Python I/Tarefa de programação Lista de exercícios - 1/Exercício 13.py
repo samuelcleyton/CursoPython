16c1355d4cc6b503093318cf849c2476168a7173
samuelcleyton <123689422+samuelcleyton@users.noreply.github.com>
2024-01-05 17:47:29 -0300

    Qtd. de números pares e ímpares

---
+++ Introdução a Ciência da Computação com Python I/Tarefa de programação Lista de exercícios - 1/Exercício 13.py
@@ -0,0 +1,24 @@
+# o programa conta a quantidade de números pares e ímpares digitados pelo usuário.
+
+terminou = False
+p = i = 0
+while (not terminou):
+    n = int(input('Digite um número, ou zero para terminar: '))
+    if n == 0:
+        terminou = True
+    else:
+        if n % 2 == 0:
+            p = p + 1
+        else:
+            i = i + 1
+
+print('P = ', p)
+print('I = ', i)
+
+
+
+
+
+
+
+
